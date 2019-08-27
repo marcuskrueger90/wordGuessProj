bdc2993e3045ebe799062e9a5c4c8e2c520e885a
Ian Applekamp <ieapplekamp@gmail.com>
2019-08-26 21:03:13 -0400

    Added the startGame function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,38 @@ var spaceLingo = [
     "spaghettification",
     "exoplant",
 ];
+
+correctGuesses = [];
+phraseGuesses = [];
+numberOfBlanks = "";
+
+var phrases = ""
+
+
+function startGame() {
+    
+    phrases = spaceLingo[Math.floor(Math.random() * spaceLingo.length)];
+    console.log(phrases);
+    
+    correctGuesses = phrases.split('');
+    console.log(correctGuesses);
+
+    for (var i = 0; i < correctGuesses.length; i++) {
+
+        if (correctGuesses[i] === " ") {
+            phraseGuesses.push("&nbsp;");
+        }
+        else {
+            phraseGuesses.push('_');
+            numberOfBlanks++;
+        }
+
+        document.getElementById('blanks').innerHTML = phraseGuesses
+
+    }
+
+    document.getElementById('blanks').innerHTML = phraseGuesses.join(' ');
+
+}
+
+startGame();
